c88baa6400991ced4a4794a64a743741aa294239
Md Najim Ahmed <najim.ju@gmail.com>
2025-03-26 20:23:33 +0800

    this is awesome, grpc integrated!

--- product-details/src/main/java/com/example/productDetails/ProductDetailsApplication.java
+++ product-details/src/main/java/com/example/productDetails/ProductDetailsApplication.java
@@ -1,5 +1,10 @@
 package com.example.productDetails;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.core.ConsoleAppender;
+import ch.qos.logback.core.encoder.Encoder;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
