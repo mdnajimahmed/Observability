4cc810b8b2d907e882ae665a1066dab46db0666e
Md Najim Ahmed <najim.ju@gmail.com>
2025-03-27 19:36:45 +0800

    wow, this is awesome!

--- product-aggregator/src/main/java/com/example/productAggregator/config/JacksonConfig.java
+++ product-aggregator/src/main/java/com/example/productAggregator/config/JacksonConfig.java
@@ -3,18 +3,9 @@ package com.example.productAggregator.config;
 import com.example.productReview.grpc.reviews.ReviewServiceGrpc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import io.grpc.ClientInterceptor;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.ServerInterceptor;
-import io.opentelemetry.api.OpenTelemetry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.annotation.Order;
-import org.springframework.grpc.client.GlobalClientInterceptor;
-import org.springframework.grpc.client.GrpcChannelFactory;
-import org.springframework.grpc.client.GrpcClient;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
